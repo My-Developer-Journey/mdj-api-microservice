e0e14b773fdc99ce13c3a603cb80d90b758765fc
diemyolo <quydiem2015@gmail.com>
2026-02-03 14:51:49 +0700

    fix maven.yml file v6

---
+++ common/src/main/java/com/diemyolo/common/configuration/DotenvConfig.java
@@ -0,0 +1,22 @@
+package com.diemyolo.common.configuration;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import jakarta.annotation.PostConstruct;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DotenvConfig {
+  @PostConstruct
+  public void loadEnv() {
+    String profile = System.getenv("SPRING_PROFILES_ACTIVE");
+    if (profile == null) {
+      profile = "dev";
+    }
+
+    String envFileName = ".env." + profile;
+
+    Dotenv dotenv = Dotenv.configure().filename(envFileName).ignoreIfMissing().load();
+
+    dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+  }
+}
