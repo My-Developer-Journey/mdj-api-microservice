8bd5f37cf2c625de307fe6b31890c2b69d738b4b
diemyolo <quydiem2015@gmail.com>
2026-02-03 14:01:05 +0700

    add multi-stage CI for github action + add postconstruct for DotenvConfig

--- admin-service/src/main/java/com/diemyolo/admin_service/configuration/DotenvConfig.java
+++ admin-service/src/main/java/com/diemyolo/admin_service/configuration/DotenvConfig.java
@@ -1,18 +1,20 @@
 package com.diemyolo.admin_service.configuration;
 
 import io.github.cdimascio.dotenv.Dotenv;
+import jakarta.annotation.PostConstruct;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class DotenvConfig {
-  static {
-    // Read from environment variable (what Docker sets)
+  @PostConstruct
+  public void loadEnv() {
     String profile = System.getenv("SPRING_PROFILES_ACTIVE");
     if (profile == null) {
       profile = "dev";
     }
 
     String envFileName = ".env." + profile;
+
     Dotenv dotenv = Dotenv.configure().filename(envFileName).ignoreIfMissing().load();
 
     dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
--- media-service/src/main/java/com/diemyolo/media_service/configuration/DotenvConfig.java
+++ media-service/src/main/java/com/diemyolo/media_service/configuration/DotenvConfig.java
@@ -1,17 +1,20 @@
 package com.diemyolo.media_service.configuration;
 
 import io.github.cdimascio.dotenv.Dotenv;
+import jakarta.annotation.PostConstruct;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class DotenvConfig {
-  static {
+  @PostConstruct
+  public void loadEnv() {
     String profile = System.getenv("SPRING_PROFILES_ACTIVE");
     if (profile == null) {
       profile = "dev";
     }
 
     String envFileName = ".env." + profile;
+
     Dotenv dotenv = Dotenv.configure().filename(envFileName).ignoreIfMissing().load();
 
     dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
